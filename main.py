f48acce9844681eaf29de7351ae550c8676c0526
Maku <makucross@gmail.com>
2017-04-25 12:02:07 -0400

    Adding base lcd stuff

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)#set board mode to Broadcom
-GPIO.setup(17,GPIO.OUT) #set up pin 17
-GPIO.setup(18,GPIO.OUT)#set up pin 18
-GPIO.output(18,1)#turn on pin 18gr
-GPIO.output(17,1)#turn on pin 17
+import Adafruit_CharLCD as LCD
+def test01:
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)#set board mode to Broadcom
+    GPIO.setup(17,GPIO.OUT) #set up pin 17
+    GPIO.setup(18,GPIO.OUT)#set up pin 18
+    GPIO.output(18,1)#turn on pin 18gr
+    GPIO.output(17,1)#turn on pin 17
+def lcdTest:
+    print("hi")
