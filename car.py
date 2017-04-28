fe8ce530fc1492c6c6423a02f028fb0247547a8d
Maku <makucross@gmail.com>
2017-04-28 13:16:51 -0400

    Transistors now working, code updated

--- car.py
+++ car.py
@@ -4,6 +4,9 @@ import _thread
 import time
 import tkinter
 from tkinter import *
+import carConf
+from carConf import *
+import RPi.GPIO as GPIO
 class Car:
     def __init__(self):
         self.master = Tk()
@@ -12,19 +15,48 @@ class Car:
         self.backwardsButton = Button(self.master,text="Backwards",command=self.backwards)
         self.turnLeftButton = Button(self.master,text="Turn Left",command=self.turnLeft)
         self.turnRightButton = Button(self.master,text="Turn right",command=self.turnRight)
+        self.stopButton = Button(self.master,text="Stop",command=self.stop)
         self.forwardButton.pack()
         self.backwardsButton.pack()
         self.turnLeftButton.pack()
         self.turnRightButton.pack()
+        self.stopButton.pack()
+        
+        GPIO.setmode(GPIO.BCM)#set board mode to Broadcom
+        for carPin in carPins:
+            pinNum = carPins[carPin]
+            GPIO.setup(pinNum,GPIO.OUT)
+            GPIO.output(pinNum,0)
         
         mainloop()
     def forward(self):
         print("Moving forwards")
+        self.changePin(pin=carPins['F'])
     def backwards(self):
         print("Moving backwards")
+        self.changePin(pin=carPins['B'])
     def turnLeft(self):
         print("Turning left")
+        self.changePin(pin=carPins['L'])
     def turnRight(self):
         print("Turning right")
+        self.changePin(pin=carPins['R'])
+    def stop(self):
+        print("Stopping")
+        self.clearPins()
+    def clearPins(self):
+        for carPin in carPins:
+            pinNum = carPins[carPin]
+            GPIO.output(pinNum,0)
+    def changePin(self,pin=None):
+        if pin==None:
+            self.clearPins()
+        else:
+            for carPin in carPins:
+                pinNum = carPins[carPin]
+                if (pin == carPin) or (pin == pinNum):
+                    GPIO.output(pinNum,1)
+                else:
+                    GPIO.output(pinNum,0)
 if __name__ == '__main__':
     car = Car()
---
+++ carConf.py
@@ -0,0 +1,5 @@
+#forwardPin = 21
+#backwardsPin = 20
+#turnLeftPin = 16
+#turnRightPin = 12
+carPins = {'F':21,'B':20,'L':16,'R':12}
